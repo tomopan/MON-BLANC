37355cf4d43f6f38845142aebf1685dc08c4c890
tomopan <tomoya.pan66@gmail.com>
2020-04-20 21:53:07 +0900

    セレクトページ作成

---
+++ resources/js/components/pages/SelectWrite.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="select-area" >
+            <p>セレクトページ</p>
+        <!-- 初めて小説を書く -->
+        <router-link
+            :to="{
+                name: 'WriteFirstTime',
+                params: { hero_id: $route.params.hero_id },
+            }"
+        >
+            <p>はじめて小説を書く</p>
+        </router-link>
+        <!-- 小説を書く -->
+        <router-link
+            :to="{
+                name: 'WriteFirstSentence',
+                params: { hero_id: $route.params.hero_id },
+            }"
+        >
+            <p>小説を書く</p>
+        </router-link>
+        </div>
+    </div>
+</template>
+
+<!-- 以下にscript/cssを記述 -->
+<script>
+// インポート
+import { mapActions, mapGetters } from "vuex";
+
+// Vueの処理
+export default {
+    components: {},
+    data() {
+        return {
+
+        };
+    },
+
+    created() {
+        //主人公データを取得
+        this.fetchHeroData(this.$route.params.hero_id);
+
+    },
+    beforeMount(){
+
+    },
+    mounted(){
+
+    },
+    computed: {
+        ...mapGetters(["HeroData"]),
+    },
+    methods: {
+        //API叩いてマッチした主人公データを取得
+        ...mapActions(["fetchHeroData"]),
+    }
+};
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/pages/WriteFirstTime.vue
@@ -0,0 +1,45 @@
+<template>
+        <div class="select-area" >
+            <p>初めて小説を書く</p>
+        </div>
+</template>
+
+<!-- 以下にscript/cssを記述 -->
+<script>
+// インポート
+import { mapActions, mapGetters } from "vuex";
+
+// Vueの処理
+export default {
+    components: {},
+    data() {
+        return {
+
+        };
+    },
+
+    created() {
+        //主人公データを取得
+        this.fetchHeroData(this.$route.params.hero_id);
+
+    },
+    beforeMount(){
+
+    },
+    mounted(){
+
+    },
+    computed: {
+        ...mapGetters(["HeroData"]),
+    },
+    methods: {
+        //API叩いてマッチした主人公データを取得
+        ...mapActions(["fetchHeroData"]),
+    }
+};
+
+</script>
+
+<style scoped>
+
+</style>
