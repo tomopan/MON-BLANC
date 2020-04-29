53e69129f8b30a32aa1a63fb9e7d7109ae635b34
tomopan <tomoya.pan66@gmail.com>
2020-04-29 15:17:30 +0900

    一時コミット

--- resources/js/components/pages/Complete.vue
+++ resources/js/components/pages/Complete.vue
@@ -1,20 +1,16 @@
 <template>
     <div class="container">
         <p>あなたの小説が完成しました！</p>
-            <router-link
-                :to="{
-                    name: 'EditPaperNovel',
-                    params: {
-                        user_paper_order:$route.params.user_paper_order
-                    }
-                }"
-            >
-                <v-img
-                    class="icon"
-                    src="img/write-page/continue.png"
-                >
-                </v-img>
-            </router-link>
+        <router-link
+            :to="{
+                name: 'EditPaperNovel',
+                params: {
+                    user_paper_order: $route.params.user_paper_order
+                }
+            }"
+        >
+            <v-img class="icon" src="img/write-page/continue.png"> </v-img>
+        </router-link>
     </div>
 </template>
 
@@ -28,38 +24,50 @@ export default {
     components: {},
     data() {
         return {
-
+            PaperNovelData:"",
         };
     },
 
     created() {
+        // 小説のデータを取得
+        this.showNovel();
         //主人公データを取得
-        this.fetchHeroData(this.$route.params.hero_id);
-
+        this.fetchHeroData(this.PaperNovelData.hero_id);
     },
-    beforeMount(){
-
-    },
-    mounted(){
-
-    },
+    beforeMount() {},
+    mounted() {},
     computed: {
-        ...mapGetters(["HeroData"]),
+        ...mapGetters(["HeroData"])
     },
     methods: {
         //API叩いてマッチした主人公データを取得
         ...mapActions(["fetchHeroData"]),
+        //ペーパーノベルの情報を取得
+        showNovel: function() {
+            axios
+                .get(
+                    "api/fetch/paper_novel/" +
+                        this.$route.params.user_paper_order
+                )
+                .then(res => {
+                    this.PaperNovelData = res.data;
+                    //タイトルの場合はtoggleをtrue
+                    if (this.PaperNovelData.title) this.title_toggle = true;
+                })
+                .catch(err => {
+                    console.log(err.response.data); //ここにエラーの箇所とどんなエラーなのか書いてあります〜（添付画像参照）
+                });
+        }
     }
 };
-
 </script>
 
 <style scoped>
-.container{
+.container {
     margin: 0 auto;
     text-align: center;
 }
-.icon{
+.icon {
     width: 100px;
 }
 </style>
