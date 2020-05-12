6088005e41b987e025f34a66e542dca270622650
Haru-maru <haru3.gaogao@gmail.com>
2020-05-12 16:14:08 +0900

    favicon更新app.blade.php編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
 </head>
 <body>
     <div id="">
